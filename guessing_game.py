b1d84a0909199bff5f17cee61ee5a2732a261300
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-07-04 06:42:37 +0800

    fixing the indicator

--- guessing_game.py
+++ guessing_game.py
@@ -2,17 +2,21 @@ __foundby__ = "zabilsabri"
 
 import random
 
-print("GUESS THE NUMBER!")
+print("GUESS THE NUMBER GAME!")
+print("Good Luck!")
 number = random.randint(1, 9)
 guess = int()
-guess_count = 0
 guess_limit = 3
 out_of_guesses = False
 
 while guess != number and not out_of_guesses:
 
-    if guess_count < guess_limit:
-        guess = int(input("\nENTER THE NUMBER: "))
+    if guess_limit != 0:
+        print("\nYou Have", end=" ")
+        print(guess_limit, end=" ")
+        print("Guesses!")
+        guess = int(input("ENTER THE NUMBER: "))
+        guess_limit -= 1
 
         if guess == number - 1 or guess == number + 1:
             print("CLOSE")
@@ -20,12 +24,11 @@ while guess != number and not out_of_guesses:
             print("LOW")
         elif guess > number:
             print("HIGH")
-        guess_count += 1
 
     else:
         out_of_guesses = True
 
 if out_of_guesses:
-    print("YOU LOSE!")
+    print("\nYOU LOSE!")
 else:
-    print("YOU WIN!!")
+    print("\nYOU WIN!!")
